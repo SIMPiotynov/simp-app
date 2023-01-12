cae51b02ae153f996ce9df0d529c89a64f8b1cc5
Nicolas MARY <nicolas.mary@conserto.pro>
2023-01-12 11:08:44 +0100

    wip

--- src/components/menu/SideMenu.js
+++ src/components/menu/SideMenu.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {createContext} from "react";
 import SideMenuItem from "./SideMenuItem";
+import logo from "../../assets/SIMP.png";
 
 export default function SideMenu() {
+    const MenuContext = React.createContext("home")
   return (
-    <div className="w-1/6 h-full bg-slate-200 py-6">
-      <h1 className="text-center font-bold text-3xl text-slate-700">SIMP</h1>
-      <div className="flex flex-col h-full py-8">
-        <SideMenuItem name="Accueil" route="/"/>
-        <SideMenuItem name="Utilisateurs" route="/users"/>
-        <SideMenuItem name="Sons" route="/sounds"/>
-      </div>
-    </div>
+      <MenuContext.Provider value="home">
+        <div className="w-1/6 h-full bg-slate-200 py-6 flex flex-col justify-between">
+            <div>
+                <h1 className="text-center font-bold text-3xl text-slate-700">Menu</h1>
+                <div className="flex flex-col py-8">
+                    <SideMenuItem name="Accueil" route="/"/>
+                    <SideMenuItem name="Utilisateurs" route="/users"/>
+                    <SideMenuItem name="Sons" route="/sounds"/>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <img src={logo} className="w-[70px]" alt="logo" />
+            </div>
+        </div>
+      </MenuContext.Provider>
   );
 }
